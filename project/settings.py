b598e9a4689000d2e87d5b5f8a2101901ea9afd7
Gulshoda <samiraqudratova809@gmail.com>
2023-04-12 18:59:31 +0500

    mai

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=jpx8fziryzs9837v#dqph&-q4n@8n0b))dme^=!p&jd#7b&@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://web-production-67a6.up.railway.app']
 
 
 # Application definition
